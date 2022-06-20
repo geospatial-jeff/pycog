b25fd9afc4f4e3b164e14de276b49702c964a80d
Jeff Albrecht <geospatialjeff@gmail.com>
2022-06-20 17:28:07 -0600

    refactor cog writer

--- pycog/writer.py
+++ pycog/writer.py
@@ -1,3 +1,5 @@
+from io import IOBase
+import struct
 import typing
 from collections import deque
 
@@ -18,57 +20,83 @@ def write_header(header: Header) -> bytes:
     )
 
 
-def write_tag(tag: Tag, endian: Endian) -> typing.Tuple[bytes, bytes]:
-    tag_code = tag.id.to_bytes(2, endian.name)
-    tag_type = tag.type.value.to_bytes(2, endian.name)
-    tag_count = tag.count.to_bytes(4, endian.name)
-
-    # We return the tag header (first 8 bytes) and tag value separately.
-    # This allows the caller to determine where values are inserted into the file.
-    # Which is especially useful when writing values larger than 4 bytes.
-    return (tag_code + tag_type + tag_count, tag.value)
-
-
-def write_ifd(ifd: IFD, endian: Endian) -> bytes:
-    # Write tags first.
-    ifd_segments = deque()
-    for tag in ifd.tags.values():
-        tag_header, tag_value = write_tag(tag, endian)
-        if len(tag_value) > 4:
-            # TODO: Implement writing of large tags
-            # Write the tag value to the end of the IFD.
-            # Update last 4 bytes of the tag to contain offset to this value.
-            continue
-        else:
-            tag_value = tag_value.ljust(4, b"\0")
-        ifd_segments.append(tag_header + tag_value)
-
-    # Prepend tag count based on how many tags we wrote.
-    tag_count = len(ifd_segments).to_bytes(2, endian.name)
-    ifd_segments.appendleft(tag_count)
-
-    # Defer writing of last 4 bytes of IFD (next IFD offset) to caller.
-    return b"".join(ifd_segments)
-
-
 def write_cog(cog: Cog) -> bytes:
-    # Strip ghost header area
+    # TODO: Store image data
+    endian = cog.header.endian
+
+    # Hard code header size to 8; skip GDAL ghost area.
     # https://gdal.org/drivers/raster/cog.html#header-ghost-area
-    cog.header.first_ifd_offset = 8
-    cog_segments = [write_header(cog.header)]
+    header_size = 8
+    cog.header.first_ifd_offset = header_size
 
-    ifd_offset = cog.header.first_ifd_offset
+    # Write the header (first 8 bytes).
+    cog_segments = [
+        write_header(cog.header)
+    ]
+
+    next_ifd_offset = cog.header.first_ifd_offset
     for idx, ifd in enumerate(cog.ifds):
-        # Write the body of each IFD.
-        ifd_bytes = write_ifd(ifd, cog.header.endian)
+        # Keep track of where we are writing large tag values.
+        large_tag_offset = 0
+        large_tag_values = []
 
-        # Write the offset to the next IFD.
-        # The last IFD in the file should have an offset of 0.
+        # Calculate the expected size of the IFD body.
+        ifd_size = 2 + (12 * len(ifd.tags)) + 4
+
+        # First 2 bytes of the IFD contains number of tags.
+        ifd_segments = []
+        ifd_segments.append(len(ifd.tags).to_bytes(2, endian.name))
+
+        # Write the tags next, 12 bytes each.
+        for tag in ifd.tags.values():
+            tag_code = tag.id.to_bytes(2, endian.name)
+            tag_type = tag.type.value.to_bytes(2, endian.name)
+            tag_count = tag.count.to_bytes(4, endian.name)
+
+            if tag.size <= 4:
+                # Short tag values (less than 4 bytes) are stored directly in the tag body.
+                tag_value = struct.pack(
+                    f"{endian.value}{tag.count}{tag.type.format}", *tag.value
+                )
+                if tag.size != 4:
+                    # Zero pad the tag value until it's 4 bytes.
+                    tag_value = tag_value.ljust(4, b"\0")
+            else:
+                # Write long tag values to the end of the IFD.
+                # The last 4 bytes of the tag become offset to the tag's value.
+                tag_value_offset = next_ifd_offset + ifd_size + large_tag_offset
+                tag_value = tag_value_offset.to_bytes(4, endian.name)
+                large_tag_values.append(
+                    struct.pack(
+                        f"{endian.value}{tag.count}{tag.type.format}", *tag.value
+                    )
+                )
+                large_tag_offset += tag.size
+
+            # Write tag to the IFD.
+            tag_segments = [tag_code, tag_type, tag_count, tag_value]
+            ifd_segments += [tag_code, tag_type, tag_count, tag_value]
+
+            # SANITY CHECK
+            assert sum(map(len, tag_segments)) == 12
+
+        # Last 4 bytes of the IFD are the offset to the next IFD.
+        # The next IFD begins after the long tag values from the previous IFD.
+        # If this is the last IFD in the file the offset should be 0.
+        # Note that image data is stored after all IFDs.
         if idx == len(cog.ifds) - 1:
-            ifd_offset = 0
+            next_ifd_offset = 0
         else:
-            ifd_offset = ifd_offset + len(ifd_bytes) + 4
-        ifd_bytes += ifd_offset.to_bytes(4, cog.header.endian.name)
-        cog_segments.append(ifd_bytes)
+            next_ifd_offset += ifd_size + large_tag_offset
+
+        ifd_segments.append(
+            next_ifd_offset.to_bytes(4, endian.name)
+        )
+
+        # Append large tag values to the end of the IFD
+        ifd_segments += large_tag_values
+
+        # Write the IFD to the COG
+        cog_segments += ifd_segments
 
     return b"".join(cog_segments)
